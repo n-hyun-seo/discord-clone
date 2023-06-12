fb9a2d81f1e0c243648c654560eae111c488eaec
Nathan Seo <seonathankk@gmail.com>
2023-06-12 00:06:38 -0700

    Style home button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import { Route, Routes, BrowserRouter as Router } from "react-router";
 import Left from "./components/left/Left";
 import Right from "./components/right/Right";
 import DiscordHomeButton from "./components/home_and_servers/DiscordHomeButton";
+import { useState, createContext, useEffect } from "react";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("home");
+  const PageContext = createContext();
+
   return (
     <div className="App">
-      <div className="servers">
-        <DiscordHomeButton />
-      </div>
+      <PageContext.Provider value={currentPage}>
+        <div className="servers">
+          <DiscordHomeButton
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </div>
+      </PageContext.Provider>
       <div className="content">
         <Left />
         <Right />
--- src/components/home_and_servers/DiscordHomeButton.jsx
+++ src/components/home_and_servers/DiscordHomeButton.jsx
@@ -1,18 +1,51 @@
 import { useState } from "react";
 
-export default function DiscordHomeButton() {
-const [highlightState, setHighlightState] = useState(true);
+export default function DiscordHomeButton(props) {
+  const [hoverState, setHoverState] = useState(false);
+
+  const currentPage = props.currentPage;
+  const setCurrentPage = props.setCurrentPage;
+
+  function checkCurrentPageOnLeave() {
+    if (currentPage === "home") return;
+    setHoverState(false);
+  }
+
+  function checkCurrentPageOnEnter() {
+    if (currentPage === "home") return;
+    setHoverState(true);
+  }
+
+  function changeBlobClass() {
+    if (currentPage === "home") return "white-blob-highlighted";
+    if (hoverState) return "white-blob-hovered";
+    if (!hoverState) return "white-blob-unhovered";
+  }
+
+  function changeButtonClass() {
+    if (currentPage === "home") return "logo-button-highlighted";
+    if (hoverState) return "logo-button-hovered";
+    if (!hoverState) return "logo-button-unhovered";
+  }
+
+  function changeLogoClass() {
+    if (currentPage === "home") return "logo-highlighted";
+    if (hoverState) return "logo-hovered";
+    if (!hoverState) return "logo-unhovered";
+  }
 
   return (
     <div className="logo-container">
-        <div className="white-blob">
-
-        </div>
-      <button className={highlightState ? "logo-button-highlighted" : "logo-button-unhighlighted"} onMouseEnter={() => setHighlightState(true)}
-      onMouseLeave={() => setHighlightState(false)}>
+      <div className={changeBlobClass()}></div>
+      <button
+        className={changeButtonClass()}
+        onMouseEnter={checkCurrentPageOnEnter}
+        onMouseLeave={checkCurrentPageOnLeave}
+        onClick={() => setCurrentPage("home")}
+      >
         <img
           src="https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6cc3c481a15a141738_icon_clyde_white_RGB.png"
-          className="logo"
+          className={changeLogoClass()}
           alt="discord logo"
         ></img>
       </button>
