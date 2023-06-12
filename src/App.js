0a13971faab5ec734ed3d47229b12a1eeb716bc5
Nathan Seo <seonathankk@gmail.com>
2023-06-11 19:07:40 -0700

    Create basic layout of sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="servers">
+      </div>
+      <div className="content">
+        <div className="left">
+          <div className="user-info"></div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Route, Routes, BrowserRouter  } from "react-router";
+import { Route, Routes, BrowserRouter as Router  } from "react-router";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
