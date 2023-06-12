7fec62959352e5bcfdc20ca59b5323dfd36d21ed
Nathan Seo <seonathankk@gmail.com>
2023-06-11 20:27:42 -0700

    Stylize input

--- src/components/left/SearchInput.jsx
+++ src/components/left/SearchInput.jsx
@@ -1,7 +1,7 @@
 export default function SearchInput(props) {
   return (
     <div className="search-input-container">
-      <input type="text" className={props.classNaming}></input>
+      <input type="text" className={props.classNaming} placeholder="Find or start a conversation"></input>
     </div>
   );
 }
