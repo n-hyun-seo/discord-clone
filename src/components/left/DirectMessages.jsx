5c50097b7ce6c18b482986a16007e4b3d8ce09b8
Nathan Seo <seonathankk@gmail.com>
2023-06-12 20:15:20 -0700

    Styled direct messages section

--- src/components/left/DirectMessages.jsx
+++ src/components/left/DirectMessages.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
 export default function DirectMessages(props) {
- return (
+  const [hoverState, setHoverState] = useState(false);
+  const dm_text = useRef();
+
+  return (
     <div className="direct-messages-container">
-        <div className="dm-header"> 
-        <p>Direct Messages</p>
+      <div
+        className="dm-header"
+        onMouseEnter={() => {
+          dm_text.current.classList.add("hovered");
+        }}
+        onMouseLeave={() => {
+          dm_text.current.classList.remove("hovered");
+        }}
+      >
+        <p ref={dm_text}>DIRECT MESSAGES</p>
         <div className="add-sign">+</div>
-        </div>   
+      </div>
+      <div className="dm-messages"> </div>
     </div>
- )
+  );
 }
