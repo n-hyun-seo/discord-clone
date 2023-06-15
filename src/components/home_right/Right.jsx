2e4939be4f27f0f607d7a1d7704a78a6e39e9cf1
Nathan Seo <seonathankk@gmail.com>
2023-06-14 17:46:04 -0700

    Added friends icon indicator on right side

--- src/components/home_right/Right.jsx
+++ src/components/home_right/Right.jsx
@@ -1,3 +1,17 @@
 export default function Right() {
-  return <div className="right"></div>;
+  return (
+    <div className="right">
+      <div className="friends-nav">
+        <div className="friends-page-indicator">
+          <img
+            src="https://www.nicepng.com/png/full/332-3327400_gillespie-fuels-propane-friend-icon-png-white.png"
+            alt="friends"
+          />
+          <p>Friends</p>
+        </div>
+        <div className="friends-divider"></div>
+      </div>
+      <div className="friends-content"></div>
+    </div>
+  );
 }
