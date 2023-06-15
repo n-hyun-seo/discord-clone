e9244141b5ecebe45186f8e72f8e525ad3b4cc2e
Nathan Seo <seonathankk@gmail.com>
2023-06-15 16:50:52 -0700

    Add user info section for servers

--- src/components/a_random_server/RandomServer.jsx
+++ src/components/a_random_server/RandomServer.jsx
@@ -1,8 +1,15 @@
+import UserInfo from "../home_left/UserInfo";
+
 export default function RandomServer() {
   return (
     <div className="content">
-      <div className="left-server"></div>
-      <div className="right-server"></div>
+      <div className="left-server">
+         <div className="channels"></div>
+         <UserInfo ImgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9O7zceDn3TKJA6lgUpjbb32m23z9P7gwCGw&usqp=CAU" />
+      </div>
+      <div className="right-server">
+
+      </div>
     </div>
   );
 }
