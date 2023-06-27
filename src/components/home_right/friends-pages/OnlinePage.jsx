d88268cc34a11b66c88db58c6e86c5e74e9820b6
Nathan Seo <seonathankk@gmail.com>
2023-06-26 23:23:31 -0700

    Apply grid layout to friends list section

--- src/components/home_right/friends-pages/OnlinePage.jsx
+++ src/components/home_right/friends-pages/OnlinePage.jsx
@@ -1,7 +1,7 @@
 export default function OnlinePage(props) {
   return (
     <div className="friends-type-container">
-      <div className="friends-type-header">{props.header}</div>
+      <div className="friends-type-header">{props.header} — 6</div>
       <div className="friends-type-list"></div>
     </div>
   );
