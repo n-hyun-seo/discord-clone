5842f1a99fef1dc0a477be908471f2ca08db8916
Nathan Seo <seonathankk@gmail.com>
2023-06-26 23:40:34 -0700

    Work with changing around display types of some elements

--- src/components/home_right/friends-pages/OnlinePage.jsx
+++ src/components/home_right/friends-pages/OnlinePage.jsx
@@ -1,8 +1,12 @@
 export default function OnlinePage(props) {
   return (
     <div className="friends-type-container">
-      <div className="friends-type-header">{props.header} — 6</div>
-      <div className="friends-type-list"></div>
+      <div className="friends-type-header">
+        <p>{props.header} — 6</p>
+      </div>
+      <div className="friends-type-list">
+
+      </div>
     </div>
   );
 }
