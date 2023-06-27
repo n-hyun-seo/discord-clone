b5bd253fec83552a88d88ef9a945ed7e89278883
Nathan Seo <seonathankk@gmail.com>
2023-06-26 22:51:03 -0700

    Add container div around elements

--- src/components/home_right/friends-pages/OnlinePage.jsx
+++ src/components/home_right/friends-pages/OnlinePage.jsx
@@ -1,6 +1,9 @@
 export default function OnlinePage(props) {
-    return <div className="friends-search-container"
-    >
-      <input className="friends-search-input" placeholder="Search"></input>
-    </div>;
-  }
+  return (
+    <div className="friends-search-container">
+      <div className="friends-search-input-container">
+        <input className="friends-search-input" placeholder="Search"></input>
+      </div>
+    </div>
+  );
+}
