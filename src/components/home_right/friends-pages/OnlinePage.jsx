3d807086825dc5a0acc475c220da7bd920db1c7b
Nathan Seo <seonathankk@gmail.com>
2023-06-27 22:48:56 -0700

    Add link to elements

--- src/components/home_right/friends-pages/OnlinePage.jsx
+++ src/components/home_right/friends-pages/OnlinePage.jsx
@@ -1,28 +1,32 @@
+import { randomFriendsList } from "./RandomFriendsList";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CurrentDMIdContext } from "../../context/CurrentDMIdContext";
+import { CurrentSectionLeftContext } from "../../context/CurrentSectionLeftContext";
+
 export default function OnlinePage(props) {
+  const [currentSectionLeft, setCurrentSectionLeft] = useContext(
+    CurrentSectionLeftContext
+  );
+  const [currentDMId, setCurrentDMId] = useContext(CurrentDMIdContext);
+
+  const onlineFriendsList = randomFriendsList.filter(
+    (user) => user.online_status !== "offline"
+  );
+
   return (
     <div className="friends-type-container">
       <div className="friends-type-header">
         <p>{props.header} — 8</p>
       </div>
       <div className="friends-type-list">
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
-        <div className="test-test">test</div>
+        {onlineFriendsList.map((user) => {
+          return (
+            <Link to={`/dm/${user.id_number}`} className="test-test">
+              <p>{user.username}</p>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
