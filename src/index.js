451c5b0a5fccca74a4d217912a7288427b68e04a
Nathan Seo <seonathankk@gmail.com>
2023-07-07 21:26:38 -0700

    Remove react query related functions and variables

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
 
-const queryClient = new QueryClient();
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
