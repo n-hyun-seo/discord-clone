ae9ca085c2654b5a78a1dbbc718a12e5542a8a67
Nathan Seo <seonathankk@gmail.com>
2023-07-11 12:27:16 -0700

    Enable React Query

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route } from "react-router";
 import "./App.css";
 import LogInPage from "./components/app-paths/auth-page/LogInPage";
 import MainPage from "./components/app-paths/MainPage";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({});
 
 function App() {
-
   return (
-    <div className="App">
-      <Routes>
-        <Route path="discord-clone" element={<LogInPage />} />
-        <Route path="discord-clone/main/*" element={<MainPage />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Routes>
+          <Route path="discord-clone" element={<LogInPage />} />
+          <Route path="discord-clone/main/*" element={<MainPage />} />
+        </Routes>
+      </div>
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
   );
 }
 
