f8814888545de68811406dbf33b6e5b6e275d228
Nathan Seo <seonathankk@gmail.com>
2023-07-12 23:54:04 -0700

    Create loading visual for friends section

---
+++ src/components/app-paths/main-page/main-page-right/friends-pages/LoadingVisual.jsx
@@ -0,0 +1,12 @@
+export default function LoadingVisual() {
+  return (
+    <div style={{width: "100%", height: "100%", display: "flex",  justifyContent: "center",
+    alignItems: "center",}}>
+      <img
+        src="https://www.istitutomarangoni.com/fe-web/img/marangoni/loader.gif"
+        alt="loading list"
+        style={{ width: "150px", marginTop: "120px" }}
+      ></img>
+    </div>
+  );
+}
