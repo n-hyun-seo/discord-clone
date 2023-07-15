178be2c71524008ad600848de4b3f92fe75db85e
Nathan Seo <seonathankk@gmail.com>
2023-07-15 01:19:04 -0700

    Change folder name and file names

---
+++ src/components/app-paths/main-page/main-page-right/friends-pages/all-users/AllUsersPage.jsx
@@ -0,0 +1,62 @@
+import { useQuery } from "@tanstack/react-query";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../../../../../../config/firebase";
+import LoadingVisual from "../LoadingVisual";
+import AllUsersPageUser from "./AllUsersPageUser";
+
+export default function AllUsersPage(props) {
+  const { isLoading, data, isError, error } = useQuery(
+    ["everyUserList"],
+    async () => {
+      let _array = [];
+      const snapshot = await getDocs(collection(db, "users"));
+      snapshot.forEach((doc) => {
+        return _array.push(String(doc.id));
+      });
+      let finalList = await Promise.all(
+        _array.map(async (uid) => {
+          const docSnapshot = await getDoc(doc(db, "users", uid));
+          const userData = await docSnapshot.data().userInfo;
+          return userData;
+        })
+      );
+      return finalList;
+    }
+  );
+
+  if (isLoading) return <LoadingVisual />;
+  if (isError) console.log(error);
+
+  let listToUse;
+
+  props.inputValue ? (listToUse = props.filteredList) : (listToUse = data);
+
+  return (
+    <section className="friends-type-container">
+      <section className="friends-type-list">
+        <div className="friends-type-header">
+          <p>
+            {props.header} — {listToUse?.length}
+          </p>
+        </div>
+        <div className="test-test"></div>
+        {listToUse?.length !== 0 ? (
+          listToUse?.map((user) => (
+            <AllUsersPageUser
+              username={user.username}
+              status={user.statusMessage}
+              ImgUrl={user.photoURL}
+              id_number={user.uid}
+              online_status={user.onlineStatus}
+              isIncoming={user.requestType}
+            />
+          ))
+        ) : (
+          <p className="no-friends-found">
+            Wumpus looked, but couldn't find anyone with that name.
+          </p>
+        )}
+      </section>
+    </section>
+  );
+}
---
+++ src/components/app-paths/main-page/main-page-right/friends-pages/all-users/AllUsersPageUser.jsx
@@ -0,0 +1,153 @@
+import { useContext, useRef, useState } from "react";
+import { CurrentSectionLeftContext } from "../../../../../../context/CurrentSectionLeftContext";
+import { CurrentDMIdContext } from "../../../../../../context/CurrentDMIdContext";
+import { CurrentUserUidContext } from "../../../../../../context/CurrentUserUidContext";
+import { useMutation } from "@tanstack/react-query";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../../../../../config/firebase";
+import { queryClient } from "../../../../../../App";
+import { Link } from "react-router-dom";
+import Online from "../../../main-page-left/direct_messages/status_icons/Online";
+import Offline from "../../../main-page-left/direct_messages/status_icons/Offline";
+import Moon from "../../../main-page-left/direct_messages/status_icons/Moon";
+import Dnd from "../../../main-page-left/direct_messages/status_icons/Dnd";
+
+export default function AddFriendPageUser(props) {
+  const [currentSectionLeft, setCurrentSectionLeft] = useContext(
+    CurrentSectionLeftContext
+  );
+  const [currentDMId, setCurrentDMId] = useContext(CurrentDMIdContext);
+  const [currentUserUid, setCurrentUserUid] = useContext(CurrentUserUidContext);
+
+  const [hoverState, setHoverState] = useState(false);
+  const [moreHoverState, setMoreHoverState] = useState(false);
+  const [messageHoverState, setMessageHoverState] = useState(false);
+
+  const moreHoverRef = useRef();
+  const moreImageRef = useRef();
+  const moreCircleRef = useRef();
+  const messageHoverRef = useRef();
+  const messageImageRef = useRef();
+  const messageCircleRef = useRef();
+
+  function changeNameClass() {
+    if (hoverState) return "user-name-dm-hovered nolimit";
+    if (!hoverState) return "user-name-dm-unhovered nolimit";
+  }
+
+  function changeDMStatusClass() {
+    if (props.status !== "") {
+      return "has-user-status-dm";
+    } else if (props.status === "") {
+      return "status-user-status-dm";
+    }
+  }
+
+  function changeDMStatusIconClass() {
+    if (props.status === "") return "no-status-icon";
+    return "status-message-icon-unhovered";
+  }
+
+  const { mutate: updateDmList } = useMutation(async () => {
+    await updateDoc(doc(db, "users", currentUserUid), {
+      directMessages: arrayUnion(props.id_number),
+    });
+
+    const userInfoSnapshot = await getDoc(doc(db, "users", props.id_number));
+    const userInfoData = await userInfoSnapshot.data().userInfo;
+
+    queryClient.setQueryData(["dmList"], (old) => {
+      if (old.filter((user) => user.uid === props.id_number).length === 0)
+        return [...old, userInfoData];
+
+      return old;
+    });
+  });
+
+  return (
+    <Link
+      to={`../../dm/${props.id_number}`}
+      className="test-test"
+      onClick={(e) => {
+        updateDmList();
+        setCurrentSectionLeft("dm");
+        setCurrentDMId(props.id_number);
+      }}
+      onMouseEnter={() => {
+        setHoverState(true);
+        messageCircleRef.current.style.backgroundColor = "#1b1c1e";
+      }}
+      onMouseLeave={() => {
+        setHoverState(false);
+        messageCircleRef.current.style.backgroundColor = "#2b2d31";
+      }}
+    >
+      <div className="pfp-container">
+        <div
+          className="pfp-circle"
+          style={{
+            backgroundImage: `url("${props.ImgUrl}")`,
+          }}
+        >
+          <div className="online-status-outer">
+            {props.online_status === "online" && <Online />}
+            {props.online_status === "offline" && <Offline />}
+            {props.online_status === "moon" && <Moon />}
+            {props.online_status === "dnd" && <Dnd />}
+          </div>
+        </div>
+      </div>
+      <section className="user-info-dm">
+        <p className={changeNameClass()}>
+          {props.username ? props.username : "error"}
+        </p>
+        <div className="user-status-dm-container">
+          <p className={changeDMStatusClass()}>
+            {props.status
+              ? props.status
+              : props.online_status === "dnd"
+              ? "Do Not Disturb"
+              : props.online_status === "online"
+              ? "Online"
+              : props.online_status === "moon"
+              ? "Away"
+              : props.online_status === "offline"
+              ? "Offline"
+              : "none"}
+          </p>
+          <img
+            src="https://icon-library.com/images/texting-icon-png/texting-icon-png-25.jpg"
+            alt="status message"
+            className={changeDMStatusIconClass()}
+          />
+        </div>
+      </section>
+      <div className="text-more-box-container">
+        <div
+          className="pfp-circle text-box"
+          ref={messageCircleRef}
+          onMouseEnter={() => {
+            setMessageHoverState(true);
+            messageHoverRef.current.classList.add("hovered");
+            messageImageRef.current.style.filter = "brightness(300%)";
+          }}
+          onMouseLeave={() => {
+            setMessageHoverState(false);
+            messageHoverRef.current.classList.remove("hovered");
+            messageImageRef.current.style.filter = "brightness(250%)";
+          }}
+        >
+          <img
+            src="https://cdn2.iconfinder.com/data/icons/interface-solid-8/2050/interface_2_glyph-23-512.png"
+            alt="chat"
+            className="text-box-dm"
+            ref={messageImageRef}
+          />
+        </div>
+        <div ref={messageHoverRef} className="message-hover-box all-users">
+          Message
+        </div>
+      </div>
+    </Link>
+  );
+}
